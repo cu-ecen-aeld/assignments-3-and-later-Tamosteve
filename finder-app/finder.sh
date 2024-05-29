b41ca41e858e361443e7dceef6fabad0dcd48c58
st693220997 <122101336+Tamosteve@users.noreply.github.com>
2024-05-29 19:44:10 +0000

    "update finder"

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -21,11 +21,11 @@ num_matching_lines=0
 list_file=$(find "$filesdir" -type f)
 
 # Parcourir chaque fichier et compter les lignes correspondantes
-while read -r file; do
+for file in $list_file; do
     matching_lines=$(grep -c "$searchstr" "$file")
-    ((num_matching_lines += matching_lines))
-    ((num_files++))
-done <<< "$list_file"
+    num_matching_lines=$((num_matching_lines + matching_lines))
+    num_files=$((num_files + 1))
+done
 
 echo "The number of files are $num_files and the number of matching lines are $num_matching_lines"
 
